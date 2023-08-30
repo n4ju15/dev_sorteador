c9896ed76c96723eb21dd78c0fef767a128ade4d
Ana Julia Gari  Albano <112116683+n4ju15@users.noreply.github.com>
2023-08-30 10:56:28 -0300

    Adding logical operators functionality.

--- scripts.js
+++ scripts.js
@@ -2,6 +2,26 @@
 function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value) // Pega o valor inserido no campo "entre"
     const max = Math.floor(document.querySelector(".input-max").value) // Pega o valor inserido no campo "e"
-   const result = Math.floor(Math.random() * (max - min + 1)) + min;
-   alert(result)
+
+    if (min >= max) { // Se o valor minímo for maior ou igual ao valor máximo exibe o alert
+        alert("O valor minímo deve ser MENOR que o valor máximo.")
+    } else { // Se não, continua o código sorteando o número e exibindo o segundo alert
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert(result)
+    }
 }
+
+/*      OUTRO EXEMPLO DE COMO PODERIA SER FEITO
+
+function generateNumber() {
+    const min = Math.ceil(document.querySelector(".input-min").value) // Pega o valor inserido no campo "entre"
+    const max = Math.floor(document.querySelector(".input-max").value) // Pega o valor inserido no campo "e"
+
+    if (max > min) { // Se o valor máximo for maior que o valor máximo faz o código
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert(result)
+    } else { // Se não, continua o código e exibe o segundo alert
+        alert("O valor minímo deve ser MENOR que o valor máximo.")
+    }
+} 
+*/
